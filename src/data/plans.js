f701617e853804bb395f36fb47bada456ac7cd4a
fatemaansari07-sketch <fatemaansari07@gmail.com>
2026-09-10 15:04:44 +0530

    Create plans.js

---
+++ src/data/plans.js
@@ -0,0 +1,11 @@
+export const TIERS = {
+  FREE: 'free',
+  PRO_99: 'pro_99',
+  VIDEO_120: 'video_120',
+  SPECIAL_COACH: 'special_coach'
+};
+
+export const CREDIT_COSTS = {
+  PRO_ANALYSIS: 10,  // 3 Keyframes = 10 Credits
+  VIDEO_RENDER: 10   // 10s Trajectory Video = 10 Credits
+};
